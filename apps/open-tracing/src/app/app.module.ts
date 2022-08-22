b9196411cc388a4f863edfd29b03bf92190f2673
Bread God <Vipcube@users.noreply.github.com>
2022-08-22 11:25:28 +0800

    Finished opentracing.

--- apps/open-tracing/src/app/app.module.ts
+++ apps/open-tracing/src/app/app.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JaegerMiddleware, JaegerModule } from "@newtral/nestjs-jaeger";
 
 @Module({
-  imports: [],
+  imports: [
+    JaegerModule.forRoot({
+      config: {
+        serviceName: 'opentracing-service',
+        reporter: {
+          logSpans: true,
+          collectorEndpoint: 'http://localhost:14268/api/traces'
+        },
+        sampler: {
+          type: 'const',
+          param: 1
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(JaegerMiddleware)
+      .forRoutes('*');
+  }
+}
