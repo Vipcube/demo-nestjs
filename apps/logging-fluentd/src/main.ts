a4da28533623595b73347c564ed39c64c7f206fc
Bread God <Vipcube@users.noreply.github.com>
2022-08-23 15:34:06 +0800

    Update port.

--- apps/logging-fluentd/src/main.ts
+++ apps/logging-fluentd/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3002;
   await app.listen(port);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   Logger.log(
