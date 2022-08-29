b0daf05ad6316c75a49d39b60f2c3eaf484cd4f9
Bread God <Vipcube@users.noreply.github.com>
2022-08-29 20:27:01 +0800

    Improve grpc server.

--- apps/grpc-client/src/app/app.module.ts
+++ apps/grpc-client/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppService } from './app.service';
 import { ClientsModule, Transport } from "@nestjs/microservices";
 import * as path from "path";
 import { HERO_PACKAGE_NAME, HEROES_SERVICE_NAME } from "@demo-nestjs/grpc-domain";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ClientsModule.registerAsync([
       {
         name: HEROES_SERVICE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:50051',
-          package: HERO_PACKAGE_NAME,
-          protoPath: path.resolve(__dirname, '../../../libs/grpc-domain/proto/hero.proto'),
-        },
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: config.get<string>('GRPC_SERVER') || 'localhost:50051',
+            package: HERO_PACKAGE_NAME,
+            protoPath: path.resolve(__dirname, '../../../libs/grpc-domain/proto/hero.proto'),
+          }
+        }),
       },
     ]),
   ],
