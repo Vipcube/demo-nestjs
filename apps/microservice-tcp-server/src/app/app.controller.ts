b21bb52ccad6e437f74583f6a83bcd8c7e756127
Bread God <Vipcube@users.noreply.github.com>
2022-09-04 22:15:30 +0800

    Improve the tcp example.

--- apps/microservice-tcp-server/src/app/app.controller.ts
+++ apps/microservice-tcp-server/src/app/app.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { MessagePattern } from "@nestjs/microservices";
+import { SERVICE_PATTERNS } from "@demo-nestjs/microservice-constraints";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern({ cmd: 'sum' })
+  @MessagePattern(SERVICE_PATTERNS.MATH_SERVICE.SUM)
   sum(data: number[]): number {
     return this.appService.sum(data);
   }
