50201311bed3f5eeb3197ec620a4ea30fbf6a07a
Bread God <Vipcube@users.noreply.github.com>
2022-09-08 23:25:45 +0800

    Simple winston with stdout and update pod labels.

--- apps/logging-winston/src/app/app.module.ts
+++ apps/logging-winston/src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppService } from './app.service';
 import { utilities, WinstonModule } from "nest-winston";
 import { version } from './../../../../package.json';
 import * as winston from "winston";
-import "winston-daily-rotate-file";
 
 @Module({
   imports: [
     WinstonModule.forRoot({
-      defaultMeta: { application: 'logging-winstom', version: version },
+      defaultMeta: { version: version },
       format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.ms(),
-        winston.format.json(),
+        utilities.format.nestLike('logging-winstom', { colors: false, prettyPrint: false }),
       ),
       transports: [
         new winston.transports.Console(),
-        new winston.transports.DailyRotateFile({
-          filename: "logs/logging-winstom-%DATE%.log",
-          datePattern: "YYYY-MM-DD",
-          maxFiles: "14d",
-        })
       ]
     })
   ],
